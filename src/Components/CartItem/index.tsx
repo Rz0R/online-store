cea7512e0b20ac037d139eb86168df690e855d72
Dmitry Ivanov <dimannisik1@gmail.com>
2022-12-17 02:43:21 +0700

    fix: increase and removal items, display cart

--- src/Components/CartItem/index.tsx
+++ src/Components/CartItem/index.tsx
@@ -1,85 +1,52 @@
-import React, { useState } from 'react';
-import { getLocalStorage, TempGetLocalStorage } from '../../Helpers/getLocalStorage';
-import setLocalStorage from '../../Helpers/setLocalStorage';
+import { useAppDispatch } from '../../hooks/redux';
+import { addCartItem, removeItemCart, dropItemCart } from '../../store/reducers/cartState';
+import { CartItem } from '../../types/data';
 import styles from './CartItem.module.scss';
 
 interface CartItemProps {
-  className: string | undefined;
-  title: string;
-  price: number;
-  images: Array<string>;
-  amount: number;
-  id: number;
-  stock: number;
-  setData: React.Dispatch<React.SetStateAction<TempGetLocalStorage>>;
+  className: string;
+  item: CartItem;
 }
 
-export default function CartItem({
-  className,
-  title,
-  price,
-  images,
-  amount,
-  id,
-  stock,
-  setData,
-}: CartItemProps) {
-  const [curAmount, setCurAmount] = useState(amount);
+export default function CartItemComp({ className, item }: CartItemProps) {
+  const dispatch = useAppDispatch();
 
-  function setAmountInLocalStorage(operation: string) {
-    const data = getLocalStorage();
-    const indexCurItem = data.cartItems.findIndex((item) => item.id === id);
-    if (operation === 'plus') {
-      data.cartItems[indexCurItem].amount += 1;
-    } else if (operation === 'minus') {
-      data.cartItems[indexCurItem].amount -= 1;
+  function handleIncQuantity() {
+    if (item.quantaty < item.stock) {
+      dispatch(addCartItem(item));
     }
-    setLocalStorage(data);
   }
 
-  function handleDecAmount() {
-    if (curAmount - 1 > 0) {
-      setCurAmount(curAmount - 1);
-      setAmountInLocalStorage('minus');
-      setData(getLocalStorage());
+  function handleDecQuantity() {
+    if (item.quantaty > 1) {
+      dispatch(removeItemCart(item.id));
     } else {
-      const data = getLocalStorage();
-      data.cartItems = data.cartItems.filter((val) => val.id !== id);
-      setLocalStorage(data);
-      setData(getLocalStorage());
-    }
-  }
-
-  function handleIncAmount() {
-    if (curAmount < stock) {
-      setCurAmount(curAmount + 1);
-      setAmountInLocalStorage('plus');
-      setData(getLocalStorage());
+      dispatch(dropItemCart(item.id));
     }
   }
 
   return (
     <div className={`${styles.item} ${className}`}>
-      <img className={styles.item__image} src={images[0]} alt="change-text" />
-      <h3 className={styles.item__title}>{title}</h3>
+      <img className={styles.item__image} src={item.images[0]} alt="change-text" />
+      <h3 className={styles.item__title}>{item.title}</h3>
       <div className={styles.price}>
         <div className={styles.price__inner}>
-          <div className={styles.price__quantity}>{curAmount}</div>
+          <div className={styles.price__quantity}>{item.quantaty}</div>
           <span className={styles.price__multi}>x</span>
-          <div className={styles.price__priceOne}>€{price}</div>
+          <div className={styles.price__priceOne}>€{item.price}</div>
         </div>
-        <div className={styles.price__totalPrice}>€{Math.floor(curAmount * price)}</div>
+        <div className={styles.price__totalPrice}>€{Math.floor(item.price * item.quantaty)}</div>
       </div>
       <div className={styles.countButtons}>
-        <button className={styles.countButtons__dec} type="button" onClick={handleDecAmount}>
+        <button className={styles.countButtons__dec} type="button" onClick={handleDecQuantity}>
           -
         </button>
-        <div className={styles.countButtons__quantity}>{curAmount}</div>
-        <button className={styles.countButtons__inc} type="button" onClick={handleIncAmount}>
+        <div className={styles.countButtons__quantity}>{item.quantaty}</div>
+        <button className={styles.countButtons__inc} type="button" onClick={handleIncQuantity}>
           +
         </button>
       </div>
-      <div className={styles.stock}>На складе: {stock} шт.</div>
+      <div className={styles.stock}>На складе: {item.stock} шт.</div>
     </div>
   );
 }
