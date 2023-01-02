bc7ec8c2bf1a661beee98231acae86eb34ac368a
Dmitry Ivanov <dimannisik1@gmail.com>
2023-01-02 22:09:21 +0700

    feat: add images filtering from duplicates

--- src/Components/ProductCard/ImageSlider/index.tsx
+++ src/Components/ProductCard/ImageSlider/index.tsx
@@ -1,5 +1,6 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Item } from '../../../types/data';
+import { IImagesWithSize } from '../../../types/imageSlider';
 import styles from './ImageSlider.module.scss';
 
 interface ImageSliderProps {
@@ -8,32 +9,53 @@ interface ImageSliderProps {
 
 export default function ImageSlider({ currentItem }: ImageSliderProps) {
   const [mainImage, setMainImage] = useState(currentItem.images[0]);
+  const [imagesWithoutDubplicate, setImagesWithoutDubplicate] = useState<IImagesWithSize[]>([]);
 
   const handleClickImage = (image: string) => {
     setMainImage(image);
   };
 
+  useEffect(() => {
+    Promise.all(currentItem.images.map((image) => fetch(image))).then((data) => {
+      const imagesWithSize = data.reduce((acc: IImagesWithSize[], val) => {
+        acc.push({
+          url: val.url,
+          size: val.headers.get('content-length'),
+        });
+
+        return acc;
+      }, []);
+
+      setImagesWithoutDubplicate(
+        imagesWithSize.filter(
+          (imageWithSize, i) =>
+            i === imagesWithSize.findIndex((val) => val.size === imageWithSize.size),
+        ),
+      );
+    });
+  }, []);
+
   return (
     <div className={styles.imageSlider}>
       <img className={styles.imageSlider__main} src={mainImage} alt={currentItem.title} />
       <ul className={styles.imageSlider__slider}>
-        {currentItem.images.map((image, i) => (
+        {imagesWithoutDubplicate.map((image, i) => (
           <li
             className={
-              mainImage !== image
+              mainImage !== image.url
                 ? styles.imageSlider__item
                 : `${styles.imageSlider__item} ${styles.imageSlider__itemActive}`
             }
-            key={image}
+            key={image.url}
           >
             <button
               className={styles.imageSlider__itemButton}
-              onClick={() => handleClickImage(image)}
+              onClick={() => handleClickImage(image.url)}
               type="button"
             >
               <img
                 className={styles.imageSlider__itemImage}
-                src={image}
+                src={image.url}
                 alt={`${currentItem.title} ${i + 1}`}
               />
             </button>
