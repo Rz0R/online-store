950fcd1ab711ff13fe447cc4774cb5ae863e68b1
Rz0R <vpa05@mail.ru>
2023-01-05 20:05:38 +1000

    docs: remove unnecessary information from readme

--- src/globals.d.ts
+++ src/globals.d.ts
@@ -1,8 +1,2 @@
 declare module '*.module.scss';
 declare module '*.jpg';
-
-declare global {
-  interface Array<T> {
-    findLastIndex(predicate: (value: T, index: number, obj: T[]) => unknown, thisArg?: any): number;
-  }
-}
